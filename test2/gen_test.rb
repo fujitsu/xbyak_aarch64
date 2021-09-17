2fe1a78acab2ee12a71608f96c3a0362f464854d
Kentaro Kawakami <kawakami.k@fujitsu.com>
2021-09-17 23:43:39 +0900

    add new test scripts

---
+++ test2/gen_test.rb
@@ -0,0 +1,239 @@
+#!/usr/bin/ruby
+#*******************************************************************************
+# Copyright 2021 FUJITSU LIMITED
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#*******************************************************************************/
+require 'byebug'
+
+class TestPatternGenerator
+  def initialize
+    @instructions_hash = {}
+    @operands_ptn = {}
+    @instructions = []
+
+    operands_ptn_init()
+  end
+
+  public
+  def parseTest(dirname)
+    # Handle all *.test files inside "dirname" directory
+    Dir.glob(dirname+'/**/*.test').each{|item|
+      puts item
+      parseTestEach(item)
+    }
+  end
+
+  def parseTestEach(filename)
+    File.open(filename, "r") do |f|
+      f.each_line{|line|
+        parseTestEachLine(line)
+      }
+    end
+  end
+
+  def parseTestEachLine(ptn_line)
+    # Remove CR and LF
+    ptn_line.chomp!
+    # Skip comment line
+    if ptn_line.start_with?("#")
+      return
+    end
+    # Skip blank line
+    if ptn_line.start_with?(/^( |\t)*$/)
+      return
+    end
+
+    # Replace first space into comma
+    # Example "ST64B x0, x0, [x0]" -> "ST64B,x0, x0, [x0]"
+    ptn_line.sub!(/ /, ",")
+
+    # Combine multiple whitespace into one, and replace all delimiters into comma
+    ptn_line.gsub!(/( |\t)+/, " ").gsub!(/, /, ",").gsub!(/ ,/, ",")
+
+    # Replace "{," into "%" temporally
+    ptn_line.gsub!(/{,/, "%")
+    ptn_line.gsub!(/ \%/, "%")
+    ptn_line.gsub!(/\% /, "%")
+
+    # Replace "<Ws>, <W(s+1)" -> "WS_PARE"
+    # Replace "<Wt>, <W(t+1)" -> "WT_PARE"
+    # Replace "<Xs>, <X(s+1)" -> "XS_PARE"
+    # Replace "<Xt>, <X(t+1)" -> "XT_PARE"
+    ptn_line.gsub!(/<Ws>,<W\(s\+1\)>/, "WS_PARE")
+    ptn_line.gsub!(/<Wt>,<W\(t\+1\)>/, "WT_PARE")
+    ptn_line.gsub!(/<Xs>,<X\(s\+1\)>/, "XS_PARE")
+    ptn_line.gsub!(/<Xt>,<X\(t\+1\)>/, "XT_PARE")
+    
+    # Split mnemonic and operands
+    STDOUT.flush
+    tmp = ptn_line.split(",")
+
+    # Recover "WS_PARE" -> "<Ws>, <W(s+1)"
+    # Recover "WT_PARE" -> "<Wt>, <W(t+1)"
+    # Recover "XS_PARE" -> "<Xs>, <X(s+1)"
+    # Recover "XT_PARE" -> "<Xt>, <X(t+1)"
+    # Recover "%" into "{,"
+    for i in 0..tmp.size-1 do
+      tmp[i].gsub!(/WS_PARE/, "<Ws>,<W(s+1)>")
+      tmp[i].gsub!(/WT_PARE/, "<Wt>,<W(t+1)>")
+      tmp[i].gsub!(/XS_PARE/, "<Xs>,<X(s+1)>")
+      tmp[i].gsub!(/XT_PARE/, "<Xt>,<X(t+1)>")
+      tmp[i].gsub!(/%/, "{,")
+    end
+
+    @instructions_hash.store(ptn_line, tmp)
+  end
+
+  def output_instruction_patterns()
+    key_list = @instructions_hash.keys
+    key_list.each{|key| output_instruction_patterns_core(key)}
+  end
+
+  def output_instruction_patterns_core(key)
+    # Example: mn = swp
+    mn        = @instructions_hash[key][0]
+    # Example: op_list = ["swp", "<Ws>", "<Wt>", "[<Xn|SP>]"]
+    op_list   = @instructions_hash[key]
+    # Example: list_size = 4
+    list_size = @instructions_hash[key].size
+    
+
+    # Generate base operands combination list
+    # Example: @base_operands_combination = ["x0", "x0", "[x0]"]
+    base_operands_combination = []
+    for i in 1..list_size-1 do
+      begin
+        base_operands_combination.push((@operands_ptn[op_list[i]])[0])
+        
+      rescue => e
+        STDOUT.flush
+        p e.class
+        p e.message
+        p e.backtrace
+        p mn
+        p i
+        p op_list[i]
+        p @operands_ptn[op_list[i]]
+        p key
+        exit(false)
+      end
+    end
+
+    # Generate operands combinations 2d-list
+    # Example: @operands_combinations =
+    #          [["x0", "x0", "[x0]"], ["x1", "x0", "[x0]"], ..., ["xzr", "x0", "[x0]"],
+    #           ["x0", "x1", "[x0]"], ["x0", "x1", "[x0]"], ..., ["x0", "xzr", "[x0]"],
+    #           ... ,
+    #           ["x0", "x0", "[x0]"], ["x0", "x0", "[x1]"], ..., ["x0", "x0", "[sp]"]]
+    operands_combinations = []
+    for i in 1..list_size-1 do
+      begin
+        num_operands_focus = @operands_ptn[op_list[i]].size
+      rescue => e
+        p e.class
+        p e.message
+        p e.backtrace
+        p mn
+        p i
+        p op_list[i]
+        exit(false)
+      end
+
+      for j in 0..num_operands_focus-1 do
+        tmp = base_operands_combination.dup
+        tmp[i-1] = (@operands_ptn[op_list[i]])[j]
+        operands_combinations.push(tmp)
+      end
+    end
+
+    # Generate instructions list
+    # Example: ["swp x0, x0, [x0]", "swp x1, x0, [x0]", ..., "swp xzr, x0, [x0]",
+    #           "swp x0, x1, [x0]", "swp x0, x1, [x0]", ..., "swp x0, xzr, [x0]",
+    #           ... ,
+    #           "swp x0, x0, [x0]", "swp x0, x0, [x1]", ..., "swp x0, x0, [sp]"]
+    for i in 0..operands_combinations.size-1 do
+      inst = mn.downcase
+
+      for j in 0..(operands_combinations[i]).size-1 do
+        if j != 0
+          inst += ","
+        else
+          inst += " "
+        end
+        inst += (operands_combinations[i])[j]
+      end
+
+      @instructions.push(inst)
+    end
+  end
+
+  def convert_for_cpp(inst)
+    inst.sub!(/ /, "(").downcase!
+    inst += "); dump();"
+    inst.sub!(/and\(/, "and_(")
+    inst.sub!(/\[/, "ptr(").sub!(/\]/, ")")
+    #puts inst
+    return inst
+  end
+  
+  def operands_ptn_init
+    @operands_ptn.store("<Ws>", ["w0", "w1", "w2", "w4", "w8", "w16", "w30", "wzr"])
+    @operands_ptn.store("<Wt>", ["w0", "w1", "w2", "w4", "w8", "w16", "w30", "wzr"])
+    @operands_ptn.store("<Xs>", ["x0", "x1", "x2", "x4", "x8", "x16", "x30", "xzr"])
+    @operands_ptn.store("<Xt>", ["x0", "x1", "x2", "x4", "x8", "x16", "x30", "xzr"])
+    @operands_ptn.store("[<Xn|SP>]", ["[x0]", "[x1]", "[x2]", "[x4]", "[x8]", "[x16]", "[x30]", "[sp]"])
+    @operands_ptn.store("[<Xn|SP>{,#0}]", ["[x0]", "[x1]", "[x2]", "[x4]", "[x8]", "[x16]", "[x30]", "[sp]"])
+    @operands_ptn.store("<Ws>,<W(s+1)>", ["w0", "w1", "w2", "w4", "w8", "w16", "w30", "wzr"])
+    @operands_ptn.store("<Wt>,<W(t+1)>", ["w0", "w1", "w2", "w4", "w8", "w16", "w30", "wzr"])
+    @operands_ptn.store("<Xs>,<X(s+1)>", ["x0", "x1", "x2", "x4", "x8", "x16", "x30", "xzr"])
+    @operands_ptn.store("<Xt>,<X(t+1)>", ["x0", "x1", "x2", "x4", "x8", "x16", "x30", "xzr"])
+    @operands_ptn.store("<Xt:St64b>", ["x0", "x2", "x6"]) # if Rt<4:3> == '11' || Rt<0> == '1' then UNDEFINED;
+  end
+  
+  def output_cpp(ofile)
+    File.open(ofile,"w") do |f|
+      f.puts "void gen() {"
+      @instructions.each{|inst|
+        tmp = inst.dup
+        #puts tmp
+        f.puts convert_for_cpp(tmp)
+      }
+      f.puts "}"
+    end
+  end
+
+  def output_asm(ofile)
+    File.open(ofile,"w") do |f|
+      @instructions.each{|inst|
+        #puts inst
+        f.puts inst
+      }
+    end
+  end
+
+  private
+end
+
+STDERR.print "Ruby 2.5.5 or higher required\n" if Gem::Version.create(RUBY_VERSION) < Gem::Version.create("2.5.5")
+pgen_all = TestPatternGenerator.new
+pgen_all.parseTest(ARGV[0])
+pgen_all.output_instruction_patterns()
+
+# Replace path name into file name
+# Example "instructions/s" -> "instructions_s"
+argv0 = ARGV[0].dup.gsub!(/\//, "_")
+
+pgen_all.output_cpp(argv0 + ".cpp")
+pgen_all.output_asm(argv0 + ".asm")
+
---
+++ test2/test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#*******************************************************************************
+# Copyright 2019-2021 FUJITSU LIMITED 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#******************************************************************************/
+AS=aarch64-linux-gnu-as
+AWK=awk
+SED=sed
+ARCH_TYPE=armv8.6-a
+CPU_TYPE=all
+CXX_FLAGS2="-std=c++11 -Wall -I../xbyak_aarch64 -DXBYAK_TEST -DXBYAK_USE_MMAP_ALLOCATOR"
+CXX=g++
+
+CPP_FILE=${1}
+
+if [ ! -f ${CPP_FILE} ] ; then
+    echo "${CPP_FILE} not found!"
+    exit 1
+fi
+
+BASE_NAME=`basename ${CPP_FILE} .cpp`
+ASM_FILE=`echo ${CPP_FILE} | sed -e "s/test_ptn_cpp/test_ptn_asm/" | sed -e "s/\.cpp//"`.asm
+echo ${ASM_FILE}
+EXP_FILE=tmp.${BASE_NAME}.exp
+ASM_O_FILE=tmp.${BASE_NAME}.asm.o
+DUMP_FILE=tmp.${BASE_NAME}.dump
+RESULT_FILE=tmp.${BASE_NAME}.result
+
+# Generate exp file
+${AS} -march=${ARCH_TYPE} -mcpu=${CPU_TYPE} -acdnl=tmp.${EXP_FILE} -o ${ASM_O_FILE} ${ASM_FILE}
+# Reorder endian 
+cat tmp.${EXP_FILE} | grep -v -e "^\*\*\*\*" | ${AWK} '{print substr($3, 7, 2) substr($3, 5, 2) substr($3, 3, 2) substr($3, 1, 2); }' | grep -v -e "^$" > ${EXP_FILE}
+
+# Generate dump file
+cat nm_frame.cpp | sed -e "s#TEST_PTN#${CPP_FILE}#" > tmp.nm_frame.${BASE_NAME}.cpp
+${CXX} ${CXX_FLAGS2} -o tmp.${BASE_NAME}.exe tmp.nm_frame.${BASE_NAME}.cpp${P_FILE} ../lib/libxbyak_aarch64.a
+./tmp.${BASE_NAME}.exe > ${DUMP_FILE}
+
+# Check result
+# Compare exp and dump
+diff -y ${DUMP_FILE} ${EXP_FILE} > ${RESULT_FILE}
