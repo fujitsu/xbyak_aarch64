542c3083b323a32b52044ca0396dfc80f14a565d
Kentaro Kawakami <kawakami.k@fujitsu.com>
2023-02-12 14:14:31 +0900

    reimplement CPU cache info.

--- sample/cpuinfo.cpp
+++ sample/cpuinfo.cpp
@@ -1,4 +1,20 @@
+/*******************************************************************************
+ * Copyright 2019-2023 FUJITSU LIMITED
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 #include <stdio.h>
+
 #include <xbyak_aarch64/xbyak_aarch64_util.h>
 
 /*
@@ -9,7 +25,9 @@ advsimd fp sve atomic
 int main() {
   using namespace Xbyak_aarch64::util;
   Cpu cpu;
-  printf("cpu type=%016lx\n", (long)cpu.getType());
+  printf("Implementer: %s\n", cpu.getImplementer());
+  printf("CPU type: %016lx\n", (long)cpu.getType());
+  printf("HW_CAP: ");
   if (cpu.has(Cpu::tADVSIMD)) {
     printf("advsimd ");
   }
@@ -24,10 +42,45 @@ int main() {
   }
   printf("\n");
   printf("# of CPU cores: %d\n", cpu.getNumCores(CoreLevel));
-  printf("Data cache level: %d\n", cpu.getDataCacheLevels());
-  printf("L1D cache size: %d\n", cpu.getDataCacheSize(0));
-  printf("L2D cache size: %d\n", cpu.getDataCacheSize(1));
-  printf("L1D cache sharing cores: %d\n", cpu.getCoresSharingDataCache(0));
-  printf("L2 cache sharing cores: %d\n", cpu.getCoresSharingDataCache(1));
+
+  const int cacheLevel = cpu.getLastDataCacheLevel();
+  printf("Data cache level: %d\n", cacheLevel);
+
+  Arm64CacheLevel levels[] = {L1, L2, L3, L4, L5, L6, L7};
+  for (int i = 0; i < cacheLevel; i++) {
+    const Arm64CacheType type = cpu.getCacheType(levels[i]);
+    const int cacheSize = cpu.getDataCacheSize(levels[i]);
+
+    switch (type) {
+    case UnifiedCache:
+      printf("L%d cache size: %d\n", i + 1, cacheSize);
+      break;
+    case DataCacheOnly:
+    case SeparateCache:
+      printf("L%dD cache size: %d\n", i + 1, cacheSize);
+      break;
+    default:
+      fprintf(stderr, "Unknown cache type=%d\n", type);
+    }
+  }
+
+  for (int i = 0; i < cacheLevel; i++) {
+    const Arm64CacheType type = cpu.getCacheType(levels[i]);
+    const int cores = cpu.getCoresSharingDataCache(levels[i]);
+
+    switch (type) {
+    case UnifiedCache:
+      printf("L%d cache sharing cores: %d\n", i + 1, cores);
+      break;
+    case DataCacheOnly:
+    case SeparateCache:
+      printf("L%dD cache sharing cores: %d\n", i + 1, cores);
+      break;
+    default:
+      fprintf(stderr, "Unknown cache type=%d\n", type);
+    }
+  }
+
   printf("\n");
+  cpu.dumpCacheInfo();
 }
--- xbyak_aarch64/xbyak_aarch64_util.h
+++ xbyak_aarch64/xbyak_aarch64_util.h
@@ -1,6 +1,6 @@
 #pragma once
 /*******************************************************************************
- * Copyright 2020-2022 FUJITSU LIMITED
+ * Copyright 2020-2023 FUJITSU LIMITED
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,17 +17,24 @@
 #ifndef XBYAK_AARCH64_UTIL_H_
 #define XBYAK_AARCH64_UTIL_H_
 
+#if !(defined(__linux__) || defined(__APPLE__) || defined(_M_ARM64))
+#error "Unsupported OS"
+#endif
+
 #include <stdint.h>
 
 namespace Xbyak_aarch64 {
 namespace util {
 typedef uint64_t Type;
 
-constexpr uint32_t maxNumberCacheLevel = 4;
+constexpr uint32_t maxCacheLevel = 7; // Specification of Armv9
 constexpr uint32_t maxTopologyLevel = 2;
 constexpr uint32_t max_path_len = 1024;
 
 enum Arm64CpuTopologyLevel { SmtLevel = 1, CoreLevel = 2 };
+enum Arm64CacheType { NoCache = 0, InstCacheOnly = 1, DataCacheOnly = 2, SeparateCache = 3, UnifiedCache = 4, OtherCache = 5 };
+enum Arm64CacheLevel { L1 = 1, L2, L3, L4, L5, L6, L7 };
+enum cacheType_t { ICache = 0, DCache = 1, UCache = 2 };
 
 enum sveLen_t {
   SVE_NONE = 0,
@@ -49,56 +56,73 @@ enum sveLen_t {
   SVE_2048 = 16 * 16,
 };
 
+enum hwCap_t {
+  XBYAK_AARCH64_HWCAP_NONE = 0,
+  XBYAK_AARCH64_HWCAP_ADVSIMD = 1 << 1,
+  XBYAK_AARCH64_HWCAP_FP = 1 << 2,
+  XBYAK_AARCH64_HWCAP_SVE = 1 << 3,
+  XBYAK_AARCH64_HWCAP_ATOMIC = 1 << 4,
+};
+
 struct implementer_t {
   uint32_t id;
   const char *implementer;
 };
 
-struct cacheInfo_t {
+/* 2023.02.11
+   cacheInfo_t does not need to be disclosed. */
+#if defined(__GNUC__) || defined(__clang_version__)
+struct __attribute__((deprecated)) cacheInfo_t {
+#elif defined(_MSC_VER)
+struct __declspec(deprecated) cacheInfo_t {
+#endif
   uint64_t midr_el1;
   uint32_t dataCacheLevel;
   uint32_t highestInnerCacheLevel;
-  uint32_t dataCacheSize[maxNumberCacheLevel];
+  uint32_t dataCacheSize[maxCacheLevel];
 };
 
 /**
    CPU detection class
 */
+class CpuInfo;
 class Cpu {
-  uint64_t type_;
-  sveLen_t sveLen_;
-
 private:
-  uint32_t coresSharingDataCache_[maxNumberCacheLevel];
-  uint32_t dataCacheSize_[maxNumberCacheLevel];
-  uint32_t dataCacheLevel_;
-  uint64_t midr_el1_;
-  uint32_t numCores_[maxTopologyLevel];
-
-  void setCacheHierarchy();
-  void setNumCores();
-  void setSysRegVal();
-  int getRegEx(char *buf, const char *path, const char *regex);
-  int getFilePathMaxTailNumPlus1(const char *path);
+  CpuInfo *info;
 
 public:
-  static const Type tNONE = 0;
-  static const Type tADVSIMD = 1 << 1;
-  static const Type tFP = 1 << 2;
-  static const Type tSVE = 1 << 3;
-  static const Type tATOMIC = 1 << 4;
+  /* 2023.02.11 */
+#if defined(__GNUC__) || defined(__clang_version__)
+  __attribute__((deperecated)) static const Type tNONE = (Type)XBYAK_AARCH64_HWCAP_NONE;
+  __attribute__((deperecated)) static const Type tADVSIMD = (Type)XBYAK_AARCH64_HWCAP_ADVSIMD;
+  __attribute__((deperecated)) static const Type tFP = (Type)XBYAK_AARCH64_HWCAP_FP;
+  __attribute__((deperecated)) static const Type tSVE = (Type)XBYAK_AARCH64_HWCAP_SVE;
+  __attribute__((deperecated)) static const Type tATOMIC = (Type)XBYAK_AARCH64_HWCAP_ATOMIC;
+#elif defined(_MSC_VER)
+  __declspec(deprecated) static const Type tNONE = (Type)XBYAK_AARCH64_HWCAP_NONE;
+  __declspec(deprecated) static const Type tADVSIMD = (Type)XBYAK_AARCH64_HWCAP_ADVSIMD;
+  __declspec(deprecated) static const Type tFP = (Type)XBYAK_AARCH64_HWCAP_FP;
+  __declspec(deprecated) static const Type tSVE = (Type)XBYAK_AARCH64_HWCAP_SVE;
+  __declspec(deprecated) static const Type tATOMIC = (Type)XBYAK_AARCH64_HWCAP_ATOMIC;
+#endif
 
   Cpu();
 
-  Type getType() const;
-  bool has(Type type) const;
-  uint64_t getSveLen() const;
-  bool isAtomicSupported() const;
+  void dumpCacheInfo() const;
+  Arm64CacheType getCacheType(const Arm64CacheLevel i) const;
+  uint32_t getCoresSharingDataCache(const Arm64CacheLevel i) const;
+  /* 2023.02.11 */
+#if defined(__GNUC__) || defined(__clang_version__)
+  uint32_t __attribute__((deprecated("Please use getLastDataCacheLevel()"))) getDataCacheLevels() const;
+#endif
+  uint32_t getDataCacheSize(const Arm64CacheLevel i) const;
   const char *getImplementer() const;
-  uint32_t getCoresSharingDataCache(uint32_t i) const;
-  uint32_t getDataCacheLevels() const;
-  uint32_t getDataCacheSize(uint32_t i) const;
+  uint32_t getLastDataCacheLevel() const;
   uint32_t getNumCores(Arm64CpuTopologyLevel level) const;
+  Type getType() const;
+  uint64_t getSveLen() const;
+  bool has(Type type) const;
+  bool isAtomicSupported() const;
 };
 
 } // namespace util
