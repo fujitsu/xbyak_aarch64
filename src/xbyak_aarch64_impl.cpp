8319610d72c1d5192366f44c32a845a7bf2642ed
Kentaro Kawakami <kawakami.k@fujitsu.com>
2023-02-14 22:33:33 +0900

    update copyright

--- src/xbyak_aarch64_impl.cpp
+++ src/xbyak_aarch64_impl.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2020-2021 FUJITSU LIMITED
+ * Copyright 2020-2023 FUJITSU LIMITED
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
