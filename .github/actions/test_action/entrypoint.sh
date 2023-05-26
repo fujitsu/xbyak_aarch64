061233fb4837957ef2f8bb0ca3c144695771928b
Kentaro Kawakami <kawakami.k@fujitsu.com>
2023-05-26 17:12:39 +0900

    [ci] use default QEMU packages of Ubuntu 20.04
    SVE is supported by >= QEMU 5.x. Ubuntu 20.04 provides QEMU 6.x.

--- .github/actions/test_action/entrypoint.sh
+++ .github/actions/test_action/entrypoint.sh
@@ -14,10 +14,12 @@
 # See the License for the specific language governing permissions and
 # imitations under the License.
 # *******************************************************************************/
-.github/automation/env/qemu.sh
 
-# Build Xbyak_aarch64
-source .github/automation/env/setenv
+# BecauseUbuntu 20.04 has qemu packages,
+#.github/automation/env/qemu.sh
+
+# Build Xbyak_aarch64 with cross compiler
+source .github/automation/env/setenv-qemu
 make -j`grep -c processor /proc/cpuinfo`
 cd test
 ./test_all.sh -q
