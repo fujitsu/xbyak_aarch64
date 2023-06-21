41e64d2dc8301e8b2aabe8e0cb5aab75220c3a1f
Kentaro Kawakami <kawakami.k@fujitsu.com>
2023-06-21 16:43:44 +0900

    [ver] update version number to v1.1.1

--- xbyak_aarch64/xbyak_aarch64_version.h
+++ xbyak_aarch64/xbyak_aarch64_version.h
@@ -1,5 +1,5 @@
 static const int majorVersion = 1;
 static const int minorVersion = 1;
-static const int patchVersion = 0;
+static const int patchVersion = 1;
 static int getVersion() { return (majorVersion << 16) + (minorVersion << 8) + patchVersion; }
-static const char *getVersionString() { return "1.1.0"; }
+static const char *getVersionString() { return "1.1.1"; }
